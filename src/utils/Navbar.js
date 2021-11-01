4797f1c32dd43ec073a5f83a73cce28d2c539e8e
addegbenga <addegbenga@gmail.com>
2021-10-31 21:53:19 -0400

    landing page done

--- src/utils/Navbar.js
+++ src/utils/Navbar.js
@@ -3,18 +3,27 @@ import { BsHandbag } from "react-icons/bs";
 export default function Navbar() {
   return (
     <div className="bg-Base mx-4">
-      <div className="flex justify-between border-b border-opacity-10 py-4">
+      <div className="flex xl:w-1/10 xl:m-auto xl:py-5 justify-between border-b border-opacity-10 py-4">
         <div className="flex items-center  ">
-          <img  className="w-44" src="/assets/navlogo.svg" alt="logo" />
+          <img className="w-44" src="/assets/navlogo.svg" alt="logo" />
         </div>
         <div className="flex items-center">
+          <ul className=" text base hidden xl:flex tracking-wider text-white ">
+            <li className="mr-8">HOME</li>
+            <li className="mr-8">ABOUT ME</li>
+            <li className="mr-8">PRACTICE AREAS</li>
+            <li className="mr-8">PAGES</li>
+          </ul>
           <BsHandbag size={28} color="white" />
-          <button className="bg-Primary ml-4 h-12 w-12  flex items-center justify-center rounded-full">
+          <button className="bg-Primary lg:hidden ml-4 h-12 w-12  flex items-center justify-center rounded-full">
             <div>
               <div className="w-7 h-0.5 bg-white"></div>
               <div className="w-7 h-0.5 bg-white my-1.5"></div>
               <div className="w-7 h-0.5 bg-white"></div>
             </div>
+          </button>
+          <button className="text-base hidden lg:block md:py-3.5 md:ml-4 px-8 lg:bg-Primary text-white">
+            CONTACT ME
           </button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "Base":"#1e2027",
-        "Primary": "#DFAE4F",
+        Base: "#1e2027",
+        Primary: "#DFAE4F",
         "Neutral-800": "#1E2027",
         "Neutral-700": "#666D79",
         "Neutral-600": "#9A9FA7",
@@ -15,6 +15,18 @@ module.exports = {
         "Neutral-300": "#D3CFC1",
         "Neutral-200": "#E4E2DB",
         "Neutral-100": "#F0EEE9",
+      },
+      width: {
+        "1/10": "87%",
+        'hero-width-bg':"790px",
+        "img-card-width":"97%",
+        "img-card-width-lg":"95%",
+      },
+      fontSize: {
+        large: "64px",
+      },
+      lineHeight: {
+        12: "4.5rem",
       },
     },
   },
